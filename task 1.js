971226d45eac92a6c3fb8bbe83f7dcbf2610f58f
OlenaAwd <elena.avdiunina@gmail.com>
2021-07-09 12:26:47 +0300

    fix mistake

--- task 1.js
+++ task 1.js
@@ -1,8 +1,8 @@
 // Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд. Значением исполнившегося промиса должно быть то кол-во миллисекунд которое передали во время вызова функции delay.
 
 const delay = ms => {
-  return new Promise (() => {
-      setTimeout(() => {logger(ms)}, ms)
+  return new Promise (resolve => {
+      setTimeout(() => resolve(ms), ms)
   })
 };
 
